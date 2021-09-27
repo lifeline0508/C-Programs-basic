3a242ce4afd822ae7f4bf6b10e90c28a5efe7139
Pragya <79352867+lifeline0508@users.noreply.github.com>
2021-09-27 18:27:22 +0530

    Add files via upload

---
+++ ugly_num.c
@@ -0,0 +1,45 @@
+/** Write a program in C to check whether a given number is an ugly number or not. **/
+
+# include <stdio.h>
+# include <string.h>
+
+int main()
+{
+int num,x=0;
+  printf("Input an integer number: ");
+  scanf("%d",&num);
+
+      if (num <= 0) {
+            printf("Input a correct number.");
+        }
+       while (num != 1)
+       {
+            if (num % 5 == 0)
+            {
+                num /= 5;
+            }
+            else if (num % 3 == 0)
+            {
+                num /= 3;
+            }
+            else if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                printf("It is not an ugly number.\n");
+                x = 1;
+                break;
+            }
+        }
+		        if (x==0)
+		        {
+                printf("It is an ugly number.\n");
+                }
+}
+
+/**
+OUTPUT
+
+**/
